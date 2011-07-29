cfc9a843067b2f76f34235bd1030f182c25d6185
Keith <user@users-MacBook-Pro.local>
2011-07-29 16:08:20 -0700

    Initial Commit

---
+++ Programming in Ob C/Programming_in_Ob_CAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  Programming_in_Ob_CAppDelegate.h
+//  Programming in Ob C
+//
+//  Created by Keith on 7/29/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Programming_in_Ob_CAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
